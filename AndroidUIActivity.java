eb2b984143070a39409690699a3c4832251b9426
Mariem SOMRANI <somranimariem8@gmail.com>
2015-09-16 23:32:07 +0000

    Create AndroidUIActivity.java

---
+++ AndroidUIActivity.java
@@ -0,0 +1,13 @@
+package tn.insat.iac.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidUIActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
